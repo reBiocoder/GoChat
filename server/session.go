a795c3e92b0ca3f2254a4148ba28baa5fa546e57
reBiocoder <1301646236@qq.com>
2021-02-25 14:50:35 +0800

    datamodel定义完成

---
+++ server/session.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"GoChat/server/store/types"
+	"container/list"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/tinode/chat/pbx"
+	"github.com/tinode/chat/server/auth"
+)
+
+// Session represents a single WS connection or a long polling session. A user may have multiple
+// sessions.
+type Session struct {
+	// protocol - NONE (unset), WEBSOCK, LPOLL, GRPC, PROXY, MULTIPLEX
+	proto SessionProto
+
+	// Session ID
+	sid string
+
+	// Websocket. Set only for websocket sessions.
+	ws *websocket.Conn
+
+	// Pointer to session's record in sessionStore. Set only for Long Poll sessions.
+	lpTracker *list.Element
+
+	// gRPC handle. Set only for gRPC clients.
+	grpcnode pbx.Node_MessageLoopServer
+
+	// Reference to the cluster node where the session has originated. Set only for cluster RPC sessions.
+	clnode *ClusterNode
+
+	// Reference to multiplexing session. Set only for proxy sessions.
+	multi        *Session
+	proxiedTopic string
+
+	// IP address of the client. For long polling this is the IP of the last poll.
+	remoteAddr string
+
+	// User agent, a string provived by an authenticated client in {login} packet.
+	userAgent string
+
+	// Protocol version of the client: ((major & 0xff) << 8) | (minor & 0xff).
+	ver int
+
+	// Device ID of the client
+	deviceID string
+	// Platform: web, ios, android
+	platf string
+	// Human language of the client
+	lang string
+	// Country code of the client
+	countryCode string
+
+	// ID of the current user. Could be zero if session is not authenticated
+	// or for multiplexing sessions.
+	uid types.Uid
+
+	// Authentication level - NONE (unset), ANON, AUTH, ROOT.
+	authLvl auth.Level
+
+	// Time when the long polling session was last refreshed
+	lastTouched time.Time
+
+	// Time when the session received any packer from client
+	lastAction int64
+
+	// Background session: subscription presence notifications and online status are delayed.
+	background bool
+	// Timer which triggers after some seconds to mark background session as foreground.
+	bkgTimer *time.Timer
+
+	// Number of subscribe/unsubscribe requests in flight.
+	inflightReqs *sync.WaitGroup
+	// Synchronizes access to session store in cluster mode:
+	// subscribe/unsubscribe replies are asynchronous.
+	sessionStoreLock sync.Mutex
+	// Indicates that the session is terminating.
+	// After this flag's been flipped to true, there must not be any more writes
+	// into the session's send channel.
+	// Read/written atomically.
+	// 0 = false
+	// 1 = true
+	terminating int32
+
+	// Outbound mesages, buffered.
+	// The content must be serialized in format suitable for the session.
+	send chan interface{}
+
+	// Channel for shutting down the session, buffer 1.
+	// Content in the same format as for 'send'
+	stop chan interface{}
+
+	// detach - channel for detaching session from topic, buffered.
+	// Content is topic name to detach from.
+	detach chan string
+
+	// Map of topic subscriptions, indexed by topic name.
+	// Don't access directly. Use getters/setters.
+	subs map[string]*Subscription
+	// Mutex for subs access: both topic go routines and network go routines access
+	// subs concurrently.
+	subsLock sync.RWMutex
+
+	// Needed for long polling and grpc.
+	lock sync.Mutex
+
+	// Field used only in cluster mode by topic master node.
+
+	// Type of proxy to master request being handled.
+	proxyReq ProxyReqType
+}
